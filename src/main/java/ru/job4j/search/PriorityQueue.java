5064e6931576c78115c9968ecfa15078d852aa6b
Nikita Migushev <migushev@gmail.com>
2023-03-26 13:41:32 +0300

    5. Очередь с приоритетом на LinkedList [#41670]

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -7,16 +7,13 @@ public class PriorityQueue {
 
     public void put(Task task) {
         int index = 0;
-        if (tasks.isEmpty()) {
-            tasks.add(index, task);
-        }
         for (Task element : tasks) {
             if (element.getPriority() > task.getPriority()) {
-                tasks.add(index, task);
                 break;
             }
             index++;
         }
+        tasks.add(index, task);
     }
 
     public Task take() {
