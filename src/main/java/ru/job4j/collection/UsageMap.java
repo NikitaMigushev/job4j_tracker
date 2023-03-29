a9805da7605a7662257b9d96361832cd3eb09d01
Nikita Migushev <migushev@gmail.com>
2023-03-30 00:01:49 +0300

    0. Map, HashMap [#212652]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -8,6 +8,8 @@ public class UsageMap {
         map.put("migushev@gmail.com", "Nikita Migushev");
         map.put("migushev@yandex.com", "Nikita Migushev");
         map.put("migushev@yandex.com", "Nikita Migushev");
+        map.put("migushev@yandex.com", "Ivan Ivan");
+        map.put("migushev@mail.ru", "Ivan Ivan");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
