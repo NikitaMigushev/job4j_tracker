254eb046763d26f78250961175b9516aa8cfc9e0
Nikita Migushev <migushev@gmail.com>
2023-03-29 23:58:16 +0300

    1. Паспорт и жители [#212656]

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -11,4 +11,13 @@ public class PassportOfficeTest {
         office.add(citizen);
         assertThat(office.get(citizen.getPassport())).isEqualTo(citizen);
     }
+
+    @Test
+    public void whenAddFalse() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.add(citizen)).isFalse();
+
+    }
 }
