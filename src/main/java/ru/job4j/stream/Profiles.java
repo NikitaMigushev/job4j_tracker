4113d80d438afdeae5ceaf561a3e73bf6aa4a505
Nikita Migushev <migushev@gmail.com>
2023-04-10 21:02:15 +0300

    6. Список адресов [#110062]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -6,10 +6,16 @@ import java.util.stream.Collectors;
 public class Profiles {
     /**
      * Method transforms list of profiles into list of addresses.
+     *
      * @param profiles
      * @return list of addresses.
      */
     public static List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(profile -> profile.getAddress()).collect(Collectors.toList());
+        return profiles.stream()
+                .map(Profile::getAddress)
+                .collect(Collectors.toList());
     }
 }
+
+
+
